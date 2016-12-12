3a408ba71bc9a9043d385e854c4b4b990f09d1c5
Owen Swerkstrom <owens@netmail.com>
2016-12-12 15:44:25 -0600

    some electron tweaks

--- electron.js
+++ electron.js
@@ -3,10 +3,15 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-      height: 600,
-      width: 800
-  });
+	mainWindow = new BrowserWindow({
+		backgroundColor: "#445",
+		fullscreen: true
+		//height: 600,
+		//width: 800
+	});
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
+app.on('window-all-closed', () => {
+  app.quit()
 });
